0d967bf6c31436a8be29956800b99e2409a9e24e
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-09-03 17:42:47 +0400

    added signup page POM

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -4,18 +4,18 @@ export class LoginPage {
   readonly page: Page;
   readonly emailInput: Locator;
   readonly passwordInput: Locator;
-  readonly submitButton: Locator;
+  readonly registerButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.emailInput = page.getByPlaceholder('Your email');
         this.passwordInput = page.getByPlaceholder('Your password');
-        this.submitButton = page.getByRole('button', { name: 'Login' });
+        this.registerButton = page.getByTestId('login-submit');
     }
 
     async login(email: string, password: string) {
         await this.emailInput.fill(email);
         await this.passwordInput.fill(password);
-        await this.submitButton.click();
+        await this.registerButton.click();
     }
 }
---
+++ pages/SignupPage.ts
@@ -0,0 +1,36 @@
+import { Page, Locator } from '@playwright/test';
+
+export class SignupPage {
+  readonly page: Page;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly dateOfBirthInput: Locator;
+  readonly countryDropdown: Locator;
+  readonly postcodeInput: Locator;
+  readonly houseNumberInput: Locator;
+  readonly streetInput: Locator;
+  readonly cityInput: Locator;
+  readonly stateInput: Locator;
+  readonly phoneNumberInput: Locator;
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly registerButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.firstNameInput = page.getByPlaceholder('First name *')
+        this.lastNameInput = page.getByPlaceholder('Your last name *')
+        this.dateOfBirthInput = page.getByPlaceholder('YYYY-MM-DD')
+        this.countryDropdown = page.getByTestId('country')
+        this.postcodeInput = page.getByPlaceholder('Your Postcode *')
+        this.houseNumberInput = page.getByPlaceholder('e.g. 42 *')
+        this.streetInput = page.getByPlaceholder('Your Street *')
+        this.cityInput = page.getByPlaceholder('Your City *')
+        this.stateInput = page.getByPlaceholder('Your State *')
+        this.phoneNumberInput = page.getByPlaceholder('Your phone *')
+        this.emailInput = page.getByPlaceholder('Your email *');
+        this.passwordInput = page.getByPlaceholder('Your password');
+        this.registerButton = page.getByTestId('register-submit');
+    }
+
+}
