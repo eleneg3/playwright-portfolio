301f69a98072deed73f9a4d5502e6b40a355b57c
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-09-04 12:12:51 +0400

    changed login function

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -4,18 +4,18 @@ export class LoginPage {
   readonly page: Page;
   readonly emailInput: Locator;
   readonly passwordInput: Locator;
-  readonly registerButton: Locator;
+  readonly loginButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.emailInput = page.getByPlaceholder('Your email');
         this.passwordInput = page.getByPlaceholder('Your password');
-        this.registerButton = page.getByTestId('login-submit');
+        this.loginButton = page.getByTestId('login-submit');
     }
 
-    async login(email: string, password: string) {
-        await this.emailInput.fill(email);
-        await this.passwordInput.fill(password);
-        await this.registerButton.click();
+    async login(user: { email: string; password: string }) {
+        await this.emailInput.fill(user.email);
+        await this.passwordInput.fill(user.password);
+        await this.loginButton.click();
     }
 }
