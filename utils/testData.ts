bfe29b3edc1355eff938f4b674844073d9bb3d94
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-09-04 12:10:20 +0400

    added account data for user login

---
+++ utils/testData.ts
@@ -0,0 +1,14 @@
+export const validUser = {
+    email: 'customer@practicesoftwaretesting.com',
+    password: 'welcome01'
+};
+
+export const invalidUserWrongEmail = {
+    email: 'wrong-email@practicesoftwaretesting.com',
+    password: 'welcome01'
+};
+
+export const invalidUserWrongPassword = {
+    email: 'customer@practicesoftwaretesting.com',
+    password: 'wrongpasword'
+};
