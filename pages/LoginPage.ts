e4972f06cffc9d2f3d10f154b1574b6e8415c37f
Elene Gotsiridze <elene.gociridze@gmail.com>
2026-09-04 12:17:59 +0400

    added selectors for error messages

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,12 +5,18 @@ export class LoginPage {
   readonly emailInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
+  readonly emailError: Locator;
+  readonly passwordError: Locator;
+  readonly wrongCredentialsError: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.emailInput = page.getByPlaceholder('Your email');
         this.passwordInput = page.getByPlaceholder('Your password');
         this.loginButton = page.getByTestId('login-submit');
+        this.emailError = page.getByTestId('email-error');
+        this.passwordError = page.getByTestId('password-error');
+        this.wrongCredentialsError = page.getByTestId('login-error');
     }
 
     async login(user: { email: string; password: string }) {
